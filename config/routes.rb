cab8048731bcb5d5c2aa946c88144f77e1a0e400
earlgrey01 <sakayori0817@yahoo.co.jp>
2020-12-27 19:54:27 +0900

    トップページ編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  get 'posts/index'
+  root "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "about" => "home#about"
+
 end
