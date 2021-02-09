26d86349c2ebe1deb11d561920a09cd93611939e
earlgrey01 <sakayori0817@yahoo.co.jp>
2021-02-09 23:58:40 +0900

    削除機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'posts/index'
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
-
   post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
 
   root "home#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
